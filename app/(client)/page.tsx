6e43b8ba48ed5ea3515caa3a927ec04c9dfa06ee
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-01-23 17:07:30 +0200

    getting posts from CMS: 1

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,7 +1,22 @@
 import Image from "next/image";
 import { client } from "@/sanity/lib/client";
 
-export default function Home() {
+async function getPosts() {
+  const query = `
+  *[_type == "post"]{
+    title,
+    slug,
+    publishedAt,
+    excerpt
+  }
+  `;
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+  console.log(posts, 'posts')
   return (
     <div>
       <h1>Hello World!</h1>
